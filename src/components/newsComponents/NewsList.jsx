931c9d00cc848db95c42b3ddc42ed8ed24b65fa3
Mario Pijuk <mpijuk00@fesb.hr>
2023-05-08 19:36:32 +0200

    Added style and logic to the news list

--- src/components/newsComponents/NewsList.jsx
+++ src/components/newsComponents/NewsList.jsx
@@ -1,9 +1,37 @@
+import styles from "../../styles/NewsList.module.css";
+import DeleteIcon from '@mui/icons-material/Delete';
+import dayjs from "dayjs";
+import axios from "axios";
+import { sortNews } from "../../utils";
 
-const NewsList = () => {
+const NewsList = ({news, refreshList, isAdmin}) => {
+
+    async function deleteNews(newsID) {
+      
+      await axios.delete(`http://localhost:3001/news/${newsID}`);
+      const result = await axios.get("http://localhost:3001/news");
+      const sortedNews = sortNews([...result.data]);
+      refreshList(sortedNews);
+    };
+
     return(
-      <p>
-        This is news List.
-      </p>
+      <div className={styles.newsListContainer}>
+        {news?.map((notification) => (
+          <div key={notification.id} className={styles.newsContainer}>
+            <div className={styles.newsHeader} style={notification.important ? {backgroundColor:"#bb6a66"} : {backgroundColor:"#66bb6a"}}>
+              <p className={styles.pTitle}>{notification.title}</p>
+              <p className={styles.pDate}>{dayjs(notification.date).format("ddd, MMM D, YYYY h:mm A")}</p>
+            </div>
+            <div className={styles.newsBody}>
+              <p className={styles.pText}>{notification.text}</p>
+              {isAdmin ? 
+                <DeleteIcon fontSize="large" className={styles.close} onClick={() => deleteNews(notification.id)}/> :
+                null
+              }
+            </div>
+          </div>               
+        ))}
+      </div>
     );
 };
 
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import NewsList from "../components/newsComponents/NewsList";
 import NewsPopUp from "../components/newsComponents/NewsPopUp";
 import { sortNews } from "../utils";
+import styles from "../styles/News.module.css";
 
 const News = ({isAdmin}) => {
     const [news, setNews] = useState([]);
@@ -16,13 +17,11 @@ const News = ({isAdmin}) => {
             });
     }, []);
 
-    console.log(news);
-
     return(
         <div>
-            <button onClick={() => setIsClicked(true)}></button>
+            <button onClick={() => setIsClicked(true)} className={styles.click}>Add news</button>
             {isClicked ? <NewsPopUp isAdmin={isAdmin} refreshList={setNews} toggle={setIsClicked}/> : null}
-            <NewsList />
+            <NewsList news={news}  refreshList={setNews} isAdmin={isAdmin}/>
         </div>
     );
 };
